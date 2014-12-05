33a11134090f5aa6090671f2dbf5eb84ed5c65f2
unknown <usrlocal@Gfixeetu142.ad.univ-angers.fr>
2014-12-05 17:34:03 +0100

    td3 server nodejs de base à vous de le personnaliser

---
+++ server/client.js
@@ -0,0 +1,38 @@
+(function($){
+
+  var socket = io.connect('http://localhost:8080');
+  	
+   $('#loginform').submit(function(event){
+    event.preventDefault();
+    socket.emit('login', {
+      username  : $('#username').val(),
+      mail    : $('#mail').val()
+    });
+  })
+
+  socket.on('newusr', function(user) {
+    $('#listusers').append('<li id=' +user.id +'>'+ user.username + '</li>');
+  });
+socket.on('logged',function(){
+    $('#login').fadeOut();
+     $('#message').focus(); //met le focus pour la saisie du message
+  });
+
+ $('#form').submit(function(event) {
+    event.preventDefault();
+    socket.emit('newmsg', {message: $('#message').val()});
+    $('#message').val(''); //pour éviter le flood...
+    $('#message').focus(); //pour remettre le focus
+  });
+ 
+  socket.on('disusr', function(user) {
+    $('#' + user.id).remove(); //on supprime l'utilisateur de la liste
+  });
+
+  socket.on('newmsg', function(message){
+    
+    var msgtpl = $('#msgtpl').html();
+    
+    $('#messages').append('<div class="message">' + Mustache.render(msgtpl,message) + '</div>');
+});
+})(jQuery);
